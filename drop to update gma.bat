a2f70de3d4d740acb1c421b20fa062fbcbdff065
Okay <1123362+otse@users.noreply.github.com>
2022-06-19 23:11:03 +0200

    added a lowered hovel

--- drop to update gma.bat
+++ drop to update gma.bat
@@ -1,2 +1,2 @@
-"D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "%~dpn1.gma" -id "2821812740" -changes "Fixed renaming map breaking the cubemap"
+"D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "%~dpn1.gma" -id "2821812740" -changes "Hovel with a lowered entrance"
 pause
---
+++ drop to update icon.bat
@@ -0,0 +1,2 @@
+"D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -icon "%~dpn1.jpg" -id "2821812740"
+pause
