37c1c9d2b262bbcd2a72620fad94f5e051b5e348
Okay <1123362+otse@users.noreply.github.com>
2022-06-20 12:09:25 +0200

    grittier wall texture

--- drop to update gma.bat
+++ drop to update gma.bat
@@ -1,2 +1,2 @@
-"D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "%~dpn1.gma" -id "2821812740" -changes "Hovel with a lowered entrance"
+"D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "%~dpn1.gma" -id "2821812740" -changes "Some small visual fixes..."
 pause
