e739cc6521442703f223b8d63e190a51ff0532f4
Okay <1123362+otse@users.noreply.github.com>
2022-06-23 17:32:38 +0200

    hdr

--- drop to update addon.bat
+++ drop to update addon.bat
@@ -1,2 +1,2 @@
-"D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "%~dpn1.gma" -id "2821812740" -changes "Added cute windows and hanging cloths."
+"D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "%~dpn1.gma" -id "2821812740" -changes "Added HDR"
 pause
