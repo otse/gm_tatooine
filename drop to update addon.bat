abbbb53ac80797fa49f16a13df145741ecf61505
Okay <1123362+otse@users.noreply.github.com>
2022-06-25 12:36:39 +0200

    new deck, lighter wall1, ripleysand

--- drop to update addon.bat
+++ drop to update addon.bat
@@ -1,2 +1,2 @@
-"D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "%~dpn1.gma" -id "2821812740" -changes "Added HDR"
+"D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "%~dpn1.gma" -id "2821812740" -changes "Set HDR to 1.0"
 pause
